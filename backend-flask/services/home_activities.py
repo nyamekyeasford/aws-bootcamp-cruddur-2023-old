500301c18c1c8eba39aff140abf101c282ad5ea4
Kelvin Nyamekye Asford <Nyamekyeasford@gmail.com>
2023-02-28 20:20:17 +0000

    main

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
 class HomeActivities:
   def run():
+    tracer = trace.get_tracer("tracer.name.here")
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
