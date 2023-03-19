9eadde83bbefb50ed28fad08c4accdf74d684c84
Kelvin Nyamekye Asford <Nyamekyeasford@gmail.com>
2023-03-19 01:53:33 +0000

    main

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
+  ('Nyamekye Asford', 'nyamekyeasford' ,'MOCK'),
   ('Andrew Bayko', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'nyamekyeasford' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
