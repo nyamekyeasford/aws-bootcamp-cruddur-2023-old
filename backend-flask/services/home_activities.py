10133e6e8367238060d2755120b94dc4462b4bb6
Kelvin Nyamekye Asford <Nyamekyeasford@gmail.com>
2023-05-23 20:43:30 +0000

    main

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -7,11 +7,11 @@ tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
-    #logger.info("HomeActivities")
-    #with tracer.start_as_current_span("home-activites-mock-data"):
-      # span = trace.get_current_span()
-      # now = datetime.now(timezone.utc).astimezone()
-      # span.set_attribute("app.now", now.isoformat())
+    logger.info("HomeActivities")
+    with tracer.start_as_current_span("home-activites-mock-data"):
+       span = trace.get_current_span()
+       now = datetime.now(timezone.utc).astimezone()
+       span.set_attribute("app.now", now.isoformat())
 
     sql = query_wrap_array("""
       SELECT
